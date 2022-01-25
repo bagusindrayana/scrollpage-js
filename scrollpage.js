a92efe6553a0553879784eb755e50bc872342248
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2022-01-25 12:29:17 +0800

    update index.html,update animation,update null optioons with menu

--- scrollpage.js
+++ scrollpage.js
@@ -194,6 +194,88 @@ class ScrollPage {
                 ? 1
                 : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
                 : (2 - Math.pow(2, -20 * x + 10)) / 2;
+            },
+            easeInCirc(x) {
+                return 1 - Math.sqrt(1 - Math.pow(x, 2));
+            },
+            easeOutCirc(x) {
+                return Math.sqrt(1 - Math.pow(x - 1, 2));
+            },
+            easeInOutCirc(x) {
+                return x < 0.5
+                  ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
+                  : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+            },
+            easeInBack(x) {
+                const c1 = 1.70158;
+                const c3 = c1 + 1;
+                
+                return c3 * x * x * x - c1 * x * x;
+            },
+            easeOutBack(x) {
+                const c1 = 1.70158;
+                const c3 = c1 + 1;
+                
+                return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+            },
+            easeInOutBack(x) {
+                const c1 = 1.70158;
+                const c2 = c1 * 1.525;
+                
+                return x < 0.5
+                  ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+                  : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+            },
+            easeInElastic(x) {
+                const c4 = (2 * Math.PI) / 3;
+                
+                return x === 0
+                  ? 0
+                  : x === 1
+                  ? 1
+                  : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
+            },
+            easeOutElastic(x) {
+                const c4 = (2 * Math.PI) / 3;
+                
+                return x === 0
+                  ? 0
+                  : x === 1
+                  ? 1
+                  : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+            },
+            easeInOutElastic(x) {
+                const c5 = (2 * Math.PI) / 4.5;
+                
+                return x === 0
+                  ? 0
+                  : x === 1
+                  ? 1
+                  : x < 0.5
+                  ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+                  : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+            },
+            easeInBounce(x) {
+                return 1 - easings["easeOutBounce"](1 - x);
+            },
+            easeOutBounce(x) {
+                const n1 = 7.5625;
+                const d1 = 2.75;
+                
+                if (x < 1 / d1) {
+                    return n1 * x * x;
+                } else if (x < 2 / d1) {
+                    return n1 * (x -= 1.5 / d1) * x + 0.75;
+                } else if (x < 2.5 / d1) {
+                    return n1 * (x -= 2.25 / d1) * x + 0.9375;
+                } else {
+                    return n1 * (x -= 2.625 / d1) * x + 0.984375;
+                }
+            },
+            easeInOutBounce(x) {
+                return x < 0.5
+                    ? (1 - easings["easeOutBounce"](1 - 2 * x)) / 2
+                    : (1 + easings["easeOutBounce"](2 * x - 1)) / 2;
             }
         };
     
@@ -259,24 +341,32 @@ class ScrollPage {
 
     moveTo(page, options = null){
         let target = null;
+        let i = 0;
         if (typeof page === 'number' ) {
-            target = this.childs[page-1];
-        } else if (typeof page === 'string' ) {
-            const i = this.findPageByNode(document.querySelector(page));
+            i = page-1;
             target = this.childs[i];
-            if(options == null && this.has(this.options?.pages,i+1) && this.options?.pages[i+1]){
-                options = this.options?.pages[i+1];
-            }
+        } else if (typeof page === 'string' ) {
+            i = this.findPageByNode(document.querySelector(page));
+            target = this.childs[i];
+            
             
         } else if (this.findPageByNode(page) !== -1) {
-            const i = this.findPageByNode(page);
+            i = this.findPageByNode(page);
             target = this.childs[i];
-            if(options == null && this.has(this.options?.pages,i+1)  && this.options?.pages[i+1]){
-                options = this.options?.pages[i+1];
-            }
+            
             
         } else {
+            
             target = document.querySelector(page);
+        }
+
+        if(options == null){
+            if(this.has(this.options?.pages,i+1)  && this.options?.pages[i+1]){
+                options = this.options?.pages[i+1];
+            } else {
+                options = this.options;
+            }
+            
         }
         
         if(target){
